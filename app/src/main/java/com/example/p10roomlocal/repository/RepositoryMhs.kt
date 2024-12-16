40f04d3a8fef2adc63185a8858984ded0bc36b5f
Nadilaaaa12 <nadilazaskiaauliaaulia@gmail.com>
2024-12-16 11:36:01 +0700

    Memberikan daftar lengkap mahasiswa yang tersimpan di database

--- app/src/main/java/com/example/p10roomlocal/repository/RepositoryMhs.kt
+++ app/src/main/java/com/example/p10roomlocal/repository/RepositoryMhs.kt
@@ -1,7 +1,11 @@
 package com.example.p10roomlocal.repository
 
 import com.example.p10roomlocal.data.entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
 
 interface RepositoryMhs {
     suspend fun insertMhs(mahasiswa: Mahasiswa)
+
+    fun getAllMhs(): Flow<List<Mahasiswa>>
+
 }
