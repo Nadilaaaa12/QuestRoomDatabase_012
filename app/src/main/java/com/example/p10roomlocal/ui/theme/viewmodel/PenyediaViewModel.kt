34f846e4ccc83be882c7521fc1c8c0d761f921a2
Nadilaaaa12 <nadilazaskiaauliaaulia@gmail.com>
2024-12-16 13:21:06 +0700

    mengatur penyediaan beberapa ViewModel melalui viewModelFactory. Setiap ViewModel diinisialisasi dengan dependensi yang diperlukan (seperti repository atau SavedStateHandle).

--- app/src/main/java/com/example/p10roomlocal/ui/theme/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/p10roomlocal/ui/theme/viewmodel/PenyediaViewModel.kt
@@ -1,16 +1,36 @@
 package com.example.p10roomlocal.ui.theme.viewmodel
 
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.p10roomlocal.KrsApp
 
 object PenyediaViewModel {
-
     val Factory = viewModelFactory {
         initializer {
             MahasiswaViewModel(
+                krsApp().containerApp.repositoryMhs
+            )
+        }
+
+        initializer {
+            HomeMhsViewModel(
+                krsApp().containerApp.repositoryMhs
+            )
+        }
+
+        initializer {
+            DetailMhsViewModel(
+                createSavedStateHandle(),
+                krsApp().containerApp.repositoryMhs
+            )
+        }
+
+        initializer {
+            UpdateMhsViewModel(
+                createSavedStateHandle(),
                 krsApp().containerApp.repositoryMhs
             )
         }
@@ -19,4 +39,3 @@ object PenyediaViewModel {
 
 fun CreationExtras.krsApp(): KrsApp =
     (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as KrsApp)
-
